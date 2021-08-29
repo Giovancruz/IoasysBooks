2eacc3018a4223f3cb1d5d99909da3426fab867a
Giovan Cruz <giovan_odin@hotmail.com>
2021-08-29 13:35:55 -0300

    wip: homepage dashboard

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Logo from "../components/Logo";
+import "./HomePage.css";
+
+import Header from "../components/Header";
+
+const HomePage = (props) => {
+  return (
+      <Logo />
+  );
+};
+
+export default HomePage;
